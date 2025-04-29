779b6c4ab4edf08682e557bf520e74dc79676d44
Ackerboy <zigzagproductions02@gmail.com>
2025-04-28 20:20:00 -0600

    Delete scriptPF.js

--- scriptPF.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.toggle-button').forEach(button => {
-  button.addEventListener('click', () => {
-    const versions = button.nextElementSibling;
-    versions.classList.toggle('hidden');
-  });
-});
